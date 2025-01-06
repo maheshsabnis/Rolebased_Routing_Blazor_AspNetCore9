e2fe31b0bd6dc69a3253300d41aaff443afe3306
mahesh.sabnis <mahesh.sabnis@imriel.com>
2025-01-06 14:55:54 +0530

    sdsd

--- Blazor_Client/Program.cs
+++ Blazor_Client/Program.cs
@@ -1,4 +1,5 @@
 using Blazor_Client;
+using Blazored.SessionStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -8,6 +9,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddBlazoredSessionStorage();
 
 
 await builder.Build().RunAsync();
